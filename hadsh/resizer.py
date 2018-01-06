06501b722bb9c400f88ddb2e1bd408ba62447da5
Stuart Longland <me@vk4msl.id.au>
2018-01-07 09:17:18 +1000

    resizer: Take avatar object, add debugging

--- hadsh/resizer.py
+++ hadsh/resizer.py
@@ -9,14 +9,17 @@ from io import BytesIO
 
 
 class ImageResizer(object):
-    def __init__(self, io_loop=None):
+    def __init__(self, log, io_loop=None):
         if io_loop is None:
             io_loop = IOLoop.current()
+        self._log = log
         self._io_loop = io_loop
         self._pool = ThreadPool()
 
     @coroutine
-    def resize(self, image_data, image_format, width, height):
+    def resize(self, avatar, width, height):
+        log = self._log.getChild('avatar[%d]' % avatar.avatar_id)
+        log.debug('Resizing to bounding box %sx%s', width, height)
         future = Future()
 
         if (width is None) and (height is None):
@@ -25,8 +28,10 @@ class ImageResizer(object):
         # Handing the value back to the coroutine
         def _on_done(result):
             if isinstance(result, tuple):
+                log.debug('Passing back exception')
                 future.set_exc_info(result)
             else:
+                log.debug('Passing back result')
                 future.set_result(result)
 
         # What to do in the thread pool
@@ -37,6 +42,8 @@ class ImageResizer(object):
                 # Get the aspect ratio
                 raw_width, raw_height = image.size
                 ratio = float(raw_width) / float(raw_height)
+                log.debug('Raw size: %dx%d, ratio %f',
+                        raw_width, raw_height, ratio)
 
                 if (ratio >= 1.0) or (height is None):
                     # Fit to width
@@ -46,6 +53,7 @@ class ImageResizer(object):
                     width = int(height * ratio)
 
                 # Scale
+                log.debug('Scaling to %dx%d', width, height)
                 image.resize((width, height), Image.LANCZOS)
 
                 # Write out result
@@ -55,14 +63,15 @@ class ImageResizer(object):
                     pil_format = 'PNG'
 
                 out_buffer = BytesIO()
+                log.debug('Saving output')
                 image.save(out_buffer, pil_format)
                 self._io_loop.add_callback(_on_done, out_buffer.getvalue())
             except:
                 self._io_loop.add_callback(_on_done, exc_info())
 
         # Run the above in the thread pool:
-        self._pool.apply_async(_do_resize, image_data,
-                image_format, width, height)
+        self._pool.apply_async(_do_resize, avatar.avatar,
+                avatar.format, width, height)
 
         # Wait for the result
         resized_data = yield future
