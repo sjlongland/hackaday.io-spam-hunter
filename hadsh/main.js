5dbba62275e48d9193e4f3a5ae0fd8e675a726a4
Stuart Longland <me@vk4msl.id.au>
2018-01-07 00:36:02 +1000

    main.js: Handle load failure

--- hadsh/main.js
+++ hadsh/main.js
@@ -12,37 +12,39 @@ var getNextPage = function() {
 	textbox.appendChild(loading_msg);
 
 	rq.onreadystatechange = function() {
-		if (this.readyState == 4 && this.status == 200) {
+		if (this.readyState == 4) {
 			// Typical action to be performed when
 			// the document is ready:
 			textbox.removeChild(loading_msg);
 
-			var data = JSON.parse(rq.responseText);
-			data.users.forEach(function (user) {
-				var userBox = document.createElement('div');
-				var avatar = document.createElement('img');
-				avatar.src = '/avatar/' + user.avatar_id;
-				avatar.class = 'avatar';
-				userBox.appendChild(avatar);
-				var profile_link = document.createElement('a');
-				profile_link.href = user.url;
-				var profile_name = document.createElement('tt');
-				profile_name.innerHTML = user.screen_name;
-				profile_link.appendChild(profile_name);
-				userBox.appendChild(profile_link);
-				var links = document.createElement('ul');
-				user.links.forEach(function (link) {
-					var link_tag = document.createElement('a');
-					link_tag.href = link.url;
-					link_tag.innerHTML = link.title;
-					var link_item = document.createElement('li');
-					link_item.appendChild(link_tag);
-					links.appendChild(link_item);
+			if (this.status === 200) {
+				var data = JSON.parse(rq.responseText);
+				data.users.forEach(function (user) {
+					var userBox = document.createElement('div');
+					var avatar = document.createElement('img');
+					avatar.src = '/avatar/' + user.avatar_id;
+					avatar.class = 'avatar';
+					userBox.appendChild(avatar);
+					var profile_link = document.createElement('a');
+					profile_link.href = user.url;
+					var profile_name = document.createElement('tt');
+					profile_name.innerHTML = user.screen_name;
+					profile_link.appendChild(profile_name);
+					userBox.appendChild(profile_link);
+					var links = document.createElement('ul');
+					user.links.forEach(function (link) {
+						var link_tag = document.createElement('a');
+						link_tag.href = link.url;
+						link_tag.innerHTML = link.title;
+						var link_item = document.createElement('li');
+						link_item.appendChild(link_tag);
+						links.appendChild(link_item);
+					});
+					userBox.appendChild(links);
+					textbox.appendChild(userBox);
 				});
-				userBox.appendChild(links);
-				textbox.appendChild(userBox);
-			});
-			textbox.appendChild(document.createElement('hr'));
+				textbox.appendChild(document.createElement('hr'));
+			}
 			page++;
 			busy = false;
 		}
