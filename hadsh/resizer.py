802db7dcf08389de8ac3fcd982ee1776a75bb3ba
Stuart Longland <me@vk4msl.id.au>
2018-01-07 09:06:03 +1000

    resizer: Add image resizer for avatars.
    This scales an image to fit within a given bounding box.

---
+++ hadsh/resizer.py
@@ -0,0 +1,70 @@
+import threading
+
+from tornado.gen import coroutine, Future, Return
+from tornado.ioloop import IOLoop
+from multiprocessing.pool import ThreadPool
+from PIL import Image
+from sys import exc_info
+from io import BytesIO
+
+
+class ImageResizer(object):
+    def __init__(self, io_loop=None):
+        if io_loop is None:
+            io_loop = IOLoop.current()
+        self._io_loop = io_loop
+        self._pool = ThreadPool()
+
+    @coroutine
+    def resize(self, image_data, image_format, width, height):
+        future = Future()
+
+        if (width is None) and (height is None):
+            raise ValueError('width and height cannot both be None')
+
+        # Handing the value back to the coroutine
+        def _on_done(result):
+            if isinstance(result, tuple):
+                future.set_exc_info(result)
+            else:
+                future.set_result(result)
+
+        # What to do in the thread pool
+        def _do_resize():
+            try:
+                image = Image.open(BytesIO(image_data))
+
+                # Get the aspect ratio
+                raw_width, raw_height = image.size
+                ratio = float(raw_width) / float(raw_height)
+
+                if (ratio >= 1.0) or (height is None):
+                    # Fit to width
+                    height = int(width / ratio)
+                else:
+                    # Fit to height
+                    width = int(height * ratio)
+
+                # Scale
+                image.resize((width, height), Image.LANCZOS)
+
+                # Write out result
+                if image_format == 'image/jpeg':
+                    pil_format = 'JPEG'
+                else:
+                    pil_format = 'PNG'
+
+                out_buffer = BytesIO()
+                image.save(out_buffer, pil_format)
+                self._io_loop.add_callback(_on_done, out_buffer.getvalue())
+            except:
+                self._io_loop.add_callback(_on_done, exc_info())
+
+        # Run the above in the thread pool:
+        self._pool.apply_async(_do_resize)
+
+        # Wait for the result
+        resized_data = yield future
+
+        # Return the data
+        raise Return(resized_data)
