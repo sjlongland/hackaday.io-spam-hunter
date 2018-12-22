d28518f56896cfbd5e0bd95c61921481425fba32
Stuart Longland <me@vk4msl.id.au>
2018-12-22 16:15:34 +1000

    traits: Add "About Me" link trait.

--- hadsh/traits/__init__.py
+++ hadsh/traits/__init__.py
@@ -1,7 +1,9 @@
 from .trait import *
 from .avatar import avatar_init
 from .spamname import spamname_init
+from .aboutmelinktrait import aboutmelink_init
 
 def init_traits(db):
     avatar_init(db)
     spamname_init(db)
+    aboutmelink_init(db)
---
+++ hadsh/traits/aboutmelinktrait.py
@@ -0,0 +1,21 @@
+from .trait import SingletonTrait, SingletonTraitInstance, \
+        UserSingletonTraitInstance
+from ..db import model
+import re
+
+
+class AboutMeLinkTrait(SingletonTrait):
+    _TRAIT_CLASS = "aboutmelink"
+
+    def _assess(self, user, log):
+        for link in user.links:
+            if link.title == user.detail.about_me:
+                return UserSingletonTraitInstance(
+                        user,
+                        SingletonTraitInstance(self),
+                        1)
+
+
+# Instantiate these instances and register them.
+def aboutmelink_init(db):
+    assert AboutMeLinkTrait(db)
