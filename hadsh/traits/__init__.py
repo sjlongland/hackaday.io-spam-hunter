aee33ef6374fc8d6d0088e7818330f74b7082d83
Stuart Longland <me@vk4msl.id.au>
2018-12-22 16:12:01 +1000

    traits: Add spam name trait

--- hadsh/traits/__init__.py
+++ hadsh/traits/__init__.py
@@ -1,5 +1,7 @@
 from .trait import *
-from .avatar import *
+from .avatar import avatar_init
+from .spamname import spamname_init
 
 def init_traits(db):
     avatar_init(db)
+    spamname_init(db)
---
+++ hadsh/traits/spamname.py
@@ -0,0 +1,30 @@
+from .trait import SingletonTrait, SingletonTraitInstance, \
+        UserSingletonTraitInstance
+from ..db import model
+import re
+
+NAME_PATTERNS = list(map(lambda p : re.compile(p),
+    [
+        # Could also be a radio call-sign, but most probably that Polish
+        # twerp.  Manually verify.
+        r'^[a-zA-Z][0-9][a-zA-Z][a-zA-Z][a-zA-Z]$',
+        # Definite spammer display name.
+        r'[0-9][a-zA-Z][0-9][0-9][0-9][a-zA-Z]$',
+    ]))
+
+
+class SpammyNameTrait(SingletonTrait):
+    _TRAIT_CLASS = "spamname"
+
+    def _assess(self, user, log):
+        for pattern in NAME_PATTERNS:
+            if pattern.match(user.screen_name):
+                return UserSingletonTraitInstance(
+                        user,
+                        SingletonTraitInstance(self),
+                        1)
+
+
+# Instantiate these instances and register them.
+def spamname_init(db):
+    assert SpammyNameTrait(db)
